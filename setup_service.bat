57bd12d5f4a326c8cf4b5c93e6b58f9094bfd4d4
unknown <nagasudhirpulla@gmail.com>
2021-08-07 20:13:51 +0530

    nssm setup

---
+++ setup_service.bat
@@ -0,0 +1,9 @@
+call nssm.exe install grafana_sms_hook "%cd%\run_server.bat"
+call nssm.exe set grafana_sms_hook AppStdout "%cd%\logs\grafana_sms_hook.log"
+call nssm.exe set grafana_sms_hook AppStderr "%cd%\logs\grafana_sms_hook.log"
+nssm set grafana_sms_hook AppRotateFiles 1
+nssm set grafana_sms_hook AppRotateOnline 1
+nssm set grafana_sms_hook AppRotateSeconds 86400
+nssm set grafana_sms_hook AppRotateBytes 10485760
+call sc start grafana_sms_hook
+rem call nssm.exe edit grafana_sms_hook
